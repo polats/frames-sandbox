eb93e9461a827c130a14079035e48132d14c57ff
Paul Gadi <paulgadi@gmail.com>
2024-05-12 23:37:33 -0400

    add machines api

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -1,5 +1,69 @@
 import { createFrames } from "frames.js/next";
+import { FramesMiddleware } from "frames.js/types";
+import { isAddress } from "viem";
+
+type TCMContext = 
+  { 
+    error?: string
+  };
+
+const SPY_API = 
+  "https://tcm-api.onrender.com/machines/"
+  //"http://localhost:4000/machines/"
+
+
+const machinesMiddleware: FramesMiddleware<any, TCMContext> = async (
+  ctx,
+  next
+) => {
+
+  // if no button pressed, this is first run so skip calling the API
+  if (!ctx.pressedButton) {
+    return next();
+  }
+
+  const stumpAddress = ctx.message?.inputText;
+
+  if (!isAddress(stumpAddress)) {
+    return next(
+      {
+        result: {
+          error: "Invalid Employee Number"
+        }
+      }
+    );
+  }
+
+  try {
+    const res = await fetch(
+      SPY_API + stumpAddress
+    );
+
+    const result = await res.json();
+
+    // const res = await fetch(
+    //   "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+    // );
+
+    // const data = await res.json();
+
+    // const {
+    //   ethereum: { usd: ethPrice },
+    // } = await res.json();
+
+
+    return next({ result });
+
+  } catch (e) {
+    console.error("Error fetching machines:", e);
+  }
+  return next();
+};
 
 export const frames = createFrames({
   basePath: "/frames",
+  initialState: {
+    pageIndex: 0,
+  },
+  middleware: [machinesMiddleware],
 });
--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -3,16 +3,57 @@ import { frames } from "./frames";
 import { Button } from "frames.js/next";
 const NEXT_PUBLIC_HOST = process.env.NEXT_PUBLIC_HOST;
 
-const handler = frames(async () => {
+
+
+const handler = frames(async (ctx) => {
+
+  let imageSrc = NEXT_PUBLIC_HOST + "/tcm.jpg";
+
+  if (ctx.result && ctx.result.error) {
+    imageSrc = 
+    <div style={
+      { 
+        width: "100%", 
+        height: "100%",
+        backgroundColor: 'black', 
+        color: 'white', 
+        padding: 32, 
+        fontSize: 50
+    }}>
+      {ctx.result.error}
+    </div>
+  }
+
+  else if (ctx.result && ctx.result.machinesInPod) {
+    imageSrc = 
+    <div style={
+      { 
+        width: "100%", 
+        height: "100%",
+        backgroundColor: 'black', 
+        color: 'white', 
+        padding: 32, 
+        fontSize: 50,
+        display: 'flex'
+    }}>
+      {
+        // show each machine in pod as a div
+        ctx.result.machinesInPod.map((machine, i) => {
+          return <div key={i}>{" [ " + machine + " ] "}</div>
+        })
+      }
+    </div>
+  }
+
   return {
-    image:  NEXT_PUBLIC_HOST + "/tcm.jpg",
+    image: imageSrc,
+    textInput: "paste stump address (0x...)",
     buttons: [
       // With query params
       <Button
         action="post"
-        target={{ pathname: "/customize", query: { foo: "bar" } }}
       >
-        START 🚀
+        SPY ON STUMP 👁️👁️
       </Button>,
     ],
   };
